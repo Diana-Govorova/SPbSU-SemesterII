891909d7a15826fff2800ba85979f7a1a6046564
Diana-Govorova <diana.govorova2001@gmail.com>
2020-04-02 20:51:13 +0300

    Added 1-3 task 1 homework.

---
+++ HW1/task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        private static int Factorial(int n)
+            => n <= 1 ? 1 : n * Factorial(n - 1);
+       
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число : ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Факториал числа {number} : {Factorial(number)}");
+        }
+    }
+}
---
+++ HW1/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        private static int CalculateFibonacciIteratively(int n)
+        {
+            int fib1 = 0;
+            int fib2 = 1;
+            switch (n)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    while (n > 1)
+                    {
+                        int tmp = fib2;
+                        fib2 = fib1 + fib2;
+                        fib1 = tmp;
+                        n--;
+                    }
+                    return fib2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число : ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Число Фиббоначи {number} : {CalculateFibonacciIteratively(number)}");
+        }
+    }
+}
