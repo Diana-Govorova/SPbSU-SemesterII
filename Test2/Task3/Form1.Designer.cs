16ea88027ba287f83652c974153eabc753563d60
Diana-Govorova <diana.govorova2001@gmail.com>
2020-06-08 13:57:58 +0300

    Added tast 1 test 1 and appveyor

---
+++ Test2/Task3/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Task3
+{
+    partial class ProgressIndicatorForm
+    {
+        /// <summary>
+        /// Designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">True if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for designer support.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.buttonStart.ForeColor = System.Drawing.Color.Black;
+            this.buttonStart.Location = new System.Drawing.Point(287, 177);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(103, 58);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(193, 103);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(291, 47);
+            this.progressBar.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // ProgressIndicatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 360);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonStart);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ProgressIndicatorForm";
+            this.Text = "ButtomProgress";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer timer;
+    }
+}
---
+++ Test2/Task3/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    /// <summary>
+    /// Progress indicator class
+    /// </summary>
+    public partial class ProgressIndicatorForm : Form
+    {
+        /// <summary>
+        /// Initialize progress indicator.
+        /// </summary>
+        public ProgressIndicatorForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Initialize progress bar.
+        /// </summary>
+        public void ProgressBarInitialize()
+        {
+            this.progressBar.Step = 1;
+            this.progressBar.Minimum = 1;
+            this.progressBar.Maximum = 100;
+        }
+
+        /// <summary>
+        /// Scale filling
+        /// </summary>
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (this.progressBar.Value == 100)
+            {
+                this.Close();
+            }
+            timer.Interval = 300;
+            this.timer.Tick += new EventHandler(this.timer_Tick);
+            timer.Start();
+        }
+
+        /// <summary>
+        /// Increases the scale of filling.
+        /// </summary>
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (this.progressBar.Value == 100)
+            {
+                this.buttonStart.Text = "Done, click to exit";
+                return;
+            }
+            this.progressBar.PerformStep();
+        }
+    }
+}
