5c4bf59d2b44753d5e19d306ea8b66a7b45588ef
Diana-Govorova <diana.govorova2001@gmail.com>
2020-11-28 22:42:49 +0300

    Fixed mistakes

--- HW7/task2/Clock.cs
+++ HW7/task2/Clock.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
+//using System.Windows.Forms.VisualStyles;
 
 namespace Task2
 {
@@ -10,13 +11,14 @@ namespace Task2
 	public partial class Clock : Form
 	{
 		private Timer timer = new Timer();
+
 		private const int width = 300;
 		private const int height = 300;
 		private const int lengthOfSecondHand = 140;
 		private const int lengthOfMinuteHand = 110;
 		private const int lengthOfHourHand = 80;
 
-		private Coordinate clockCenter = new Coordinate(200, 160);
+		private Coordinate centerCoordinate;
 
 		private Bitmap bitmap;
 		private Graphics graphics;
@@ -49,43 +51,30 @@ namespace Task2
 		public Clock()
 		{
 			InitializeComponent();
-			//ClockFace();
-			//timer.Interval = 1000;
-			//timer.Tick += new EventHandler(this.Tick);
-			//timer.Start();
-		}
-
-		/// <summary>
-		/// Draws the clock face and initializes clock hands.
-		/// </summary>
-		private void ClockFormLoad(object sender, EventArgs e)
-		{
-			bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-
-			graphics = Graphics.FromImage(bitmap);
-
+			bitmap = new Bitmap(width + 1, height + 1);
+			centerCoordinate = new Coordinate();
+			centerCoordinate.xCoordinate = width / 2;
+			centerCoordinate.yCoordinate = height / 2;
 			ClockFace();
-
-			Tick(timer, EventArgs.Empty);
+			timer.Interval = 1000;
+			timer.Tick += new EventHandler(this.Tick);
+			timer.Start();
 		}
-			//secondHand = new Hand(new Point(0, -20), new Point(0, radius - 10), new Pen(Brushes.Red, 1));
-			//minuteHand = new Hand(new Point(0, -15), new Point(0, radius - 20), new Pen(Brushes.Black, 2));
-			//hourHand = new Hand(new Point(0, -10), new Point(0, radius - 60), new Pen(Brushes.Gray, 5));
-
-
 
 		/// <summary>
 		/// Draw a clock's face.
 		/// </summary>
 		private void ClockFace()
 		{
-			graphics.DrawEllipse(new Pen(Color.Black, 3), 50, 20, width, height);
+			graphics = Graphics.FromImage(bitmap);
+			graphics.DrawEllipse(new Pen(Color.Black, 1f), 0, 0, width, height);
 
-			graphics.DrawString("12", new Font("Arial", 12), Brushes.Black, new PointF(190, 24));
-			graphics.DrawString("3", new Font("Arial", 12), Brushes.Black, new PointF(330, 150));
-			graphics.DrawString("6", new Font("Arial", 12), Brushes.Black, new PointF(190, 290));
-			graphics.DrawString("9", new Font("Arial", 12), Brushes.Black, new PointF(60, 150));
+			graphics.DrawString("12", new Font("Arial", 12), Brushes.Black, new PointF(140, 2));
+			graphics.DrawString("3", new Font("Arial", 12), Brushes.Black, new PointF(286, 140));
+			graphics.DrawString("6", new Font("Arial", 12), Brushes.Black, new PointF(142, 282));
+			graphics.DrawString("9", new Font("Arial", 12), Brushes.Black, new PointF(0, 140));
 
+			Tick(timer, EventArgs.Empty);
 		}
 
 		/// <summary>
@@ -93,21 +82,23 @@ namespace Task2
 		/// </summary>
 		private void Tick(object sender, EventArgs e)
 		{
-			Bitmap tickBitmap = (Bitmap)bitmap.Clone();
+			var tickBitmap = (Bitmap)bitmap.Clone();
 			graphics = Graphics.FromImage(tickBitmap);
 
 			int second = DateTime.Now.Second;
 			int minute = DateTime.Now.Minute;
 			int hour = DateTime.Now.Hour;
 
-			var handCoordinate = MinuteOrSecondHandCoordinate(second, lengthOfSecondHand);
-			graphics.DrawLine(new Pen(Color.Red, 1f), new Point(clockCenter.xCoordinate, clockCenter.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
+			var handCoordinate = new Coordinate();
+
+			handCoordinate = MinuteOrSecondHandCoordinate(second, lengthOfSecondHand);
+			graphics.DrawLine(new Pen(Color.Red, 1f), new Point(centerCoordinate.xCoordinate, centerCoordinate.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
 
 			handCoordinate = MinuteOrSecondHandCoordinate(minute, lengthOfMinuteHand);
-			graphics.DrawLine(new Pen(Color.Black, 2f), new Point(clockCenter.xCoordinate, clockCenter.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
+			graphics.DrawLine(new Pen(Color.Black, 2f), new Point(centerCoordinate.xCoordinate, centerCoordinate.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
 
 			handCoordinate = HourHandCoordinate(hour % 12, minute, lengthOfHourHand);
-			graphics.DrawLine(new Pen(Color.Black, 3f), new Point(clockCenter.xCoordinate, clockCenter.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
+			graphics.DrawLine(new Pen(Color.Black, 3f), new Point(centerCoordinate.xCoordinate, centerCoordinate.yCoordinate), new Point(handCoordinate.xCoordinate, handCoordinate.yCoordinate));
 
 			pictureBox1.Image = tickBitmap;
 		}
@@ -125,13 +116,13 @@ namespace Task2
 
 			if (value >= 0 && value <= 180)
 			{
-				coordinate.xCoordinate = clockCenter.xCoordinate + (int)(handLength * Math.Sin(Math.PI * value / 180));
-				coordinate.yCoordinate = clockCenter.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
+				coordinate.xCoordinate = centerCoordinate.xCoordinate + (int)(handLength * Math.Sin(Math.PI * value / 180));
+				coordinate.yCoordinate = centerCoordinate.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
 			}
 			else
 			{
-				coordinate.xCoordinate = clockCenter.xCoordinate - (int)(handLength * -Math.Sin(Math.PI * value / 180));
-				coordinate.yCoordinate = clockCenter.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
+				coordinate.xCoordinate = centerCoordinate.xCoordinate - (int)(handLength * -Math.Sin(Math.PI * value / 180));
+				coordinate.yCoordinate = centerCoordinate.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
 			}
 			return coordinate;
 		}
@@ -150,13 +141,13 @@ namespace Task2
 
 			if (value >= 0 && value <= 180)
 			{
-				coordinate.xCoordinate = clockCenter.xCoordinate + (int)(handLength * Math.Sin(Math.PI * value / 180));
-				coordinate.yCoordinate = clockCenter.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
+				coordinate.xCoordinate = centerCoordinate.xCoordinate + (int)(handLength * Math.Sin(Math.PI * value / 180));
+				coordinate.yCoordinate = centerCoordinate.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
 			}
 			else
 			{
-				coordinate.xCoordinate = clockCenter.xCoordinate - (int)(handLength * -Math.Sin(Math.PI * value / 180));
-				coordinate.yCoordinate = clockCenter.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
+				coordinate.xCoordinate = centerCoordinate.xCoordinate - (int)(handLength * -Math.Sin(Math.PI * value / 180));
+				coordinate.yCoordinate = centerCoordinate.yCoordinate - (int)(handLength * Math.Cos(Math.PI * value / 180));
 			}
 			return coordinate;
 		}
