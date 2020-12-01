84f28a608f14c0a1ce70820c0399a1dc1510c5b2
Diana-Govorova <diana.govorova2001@gmail.com>
2020-12-01 20:33:24 +0300

    Add task1 and test for it

---
+++ FinalTest2/IntZeroChecker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FinalTest2
+{
+	/// <summary>
+	/// Class with checks if int is zero.
+	/// </summary>
+	public class IntZeroChecker : IZeroChecker
+	{
+		/// <summary>
+		/// Checks if int is zero.
+		/// </summary>
+		public bool IsZero(object o)
+		{
+			if (!(o is int) || o == null)
+			{
+				throw new ArgumentException();
+			}
+
+			var that = o as int?;
+
+			if (that == 0)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
---
+++ FinalTest2/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FinalTest2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
